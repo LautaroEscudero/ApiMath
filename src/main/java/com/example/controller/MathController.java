3946534194cd324878155382ce16b96004928686
Lautaro Escudero <lauta66@gmail.com>
2020-10-10 13:45:30 -0300

    catching exception in post method

--- src/main/java/com/example/controller/MathController.java
+++ src/main/java/com/example/controller/MathController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,12 +25,22 @@ public class MathController {
 	@PostMapping("/")
 	public ResponseEntity<?> resultByPost(
 			@RequestBody ObjectNode body) {
+		
+		try {
+			
+			Integer precision = null;
+			
+			if (body.hasNonNull("precision") && body.get("precision").isInt())
+				precision = body.get("precision").asInt();
 
-		Integer precision = null;
-		if (body.hasNonNull("precision") && body.get("precision").isInt())
-			precision = body.get("precision").asInt();
+			return mathService.getResult(body.get("expression").asText(), precision);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			return new ResponseEntity<>("Something went swrong " + e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 
-		return mathService.getResult(body.get("expression").asText(), precision);
+		
 		
 	}
 
@@ -44,10 +55,14 @@ public class MathController {
 
 	// Add symbol + to the math expression 
 	// Only use if URL is not encoded
+	
 	private String filter(String expression) {
+		
 		StringBuilder newExpression = new StringBuilder();
 		newExpression.append(expression);
+		
 		for (int i = 0; i < newExpression.length(); i++) {
+			
 			if (newExpression.charAt(i) >= 48 && newExpression.charAt(i) <= 57 || newExpression.charAt(i) == 40 ) {
 				if (i > 0 && newExpression.charAt(i - 1) == 32) {
 					newExpression.setCharAt(i - 1, '+');
